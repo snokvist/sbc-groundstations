e8384aac78b367c7f5a9cfc11916634a00adb8ec
Joakim <joakim.snokvist@gmail.com>
2025-12-16 20:01:07 +0100

    Update WFB supervisor installation commands

--- package/wfb_supervisor/wfb_supervisor.mk
+++ package/wfb_supervisor/wfb_supervisor.mk
@@ -15,7 +15,9 @@ define WFB_SUPERVISOR_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/wfb_supervisor $(TARGET_DIR)/usr/bin/wfb_supervisor
 	$(INSTALL) -D -m 0755 $(@D)/scripts/monitor.sh $(TARGET_DIR)/usr/bin/monitor.sh
 	$(INSTALL) -D -m 0755 $(@D)/scripts/shaper.sh $(TARGET_DIR)/usr/bin/shaper.sh
-	$(INSTALL) -D -m 0644 $(@D)/config/wfb.conf $(TARGET_DIR)/etc/wfb.conf
+#	$(INSTALL) -D -m 0644 $(@D)/config/wfb.conf $(TARGET_DIR)/etc/wfb.conf
+	$(INSTALL) -D -m 0644 $(@D)/config/wfb-aggregator.conf $(TARGET_DIR)/etc/wfb.conf
+
 endef
 
 $(eval $(generic-package))
