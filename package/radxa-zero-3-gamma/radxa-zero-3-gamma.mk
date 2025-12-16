85f8cebc76dd10053868601dba60fd6f9e825e22
Joakim <joakim.snokvist@gmail.com>
2025-12-16 18:59:38 +0100

    Define radxa-zero3-gamma package configuration
    Added build and installation commands for radxa-zero3-gamma package.

--- package/radxa-zero-3-gamma/radxa-zero-3-gamma.mk
+++ package/radxa-zero-3-gamma/radxa-zero-3-gamma.mk
@@ -1,1 +1,36 @@
+################################################################################
+#
+# radxa-zero3-gamma
+#
+################################################################################
+
+RADXA_ZERO3_GAMMA_VERSION = main
+RADXA_ZERO3_GAMMA_SITE = https://github.com/snokvist/radxa-zero3-gamma.git
+RADXA_ZERO3_GAMMA_SITE_METHOD = git
+RADXA_ZERO3_GAMMA_LICENSE = Proprietary
+
+RADXA_ZERO3_GAMMA_DEPENDENCIES = libdrm
+
+define RADXA_ZERO3_GAMMA_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) \
+		CC="$(TARGET_CC)" \
+		PKG_CONFIG="$(PKG_CONFIG_HOST_BINARY)" \
+		DEFAULT_CRTC="$(BR2_PACKAGE_RADXA_ZERO3_GAMMA_DEFAULT_CRTC)" \
+		PREFIX="/usr" \
+		CFLAGS="$(TARGET_CFLAGS)" \
+		CPPFLAGS="$(TARGET_CPPFLAGS)" \
+		LDFLAGS="$(TARGET_LDFLAGS)"
+endef
+
+define RADXA_ZERO3_GAMMA_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) \
+		DESTDIR="$(TARGET_DIR)" \
+		PREFIX="/usr" \
+		install
+ifeq ($(BR2_PACKAGE_RADXA_ZERO3_GAMMA_INSTALL_PRESETS),y)
+	$(INSTALL) -D -m 0644 $(@D)/presets.ini $(TARGET_DIR)/etc/gamma-presets.ini
+endif
+endef
+
+$(eval $(generic-package))
 
