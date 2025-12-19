8e126275087c17adb8a4019a7f81864369db8dd7
Joakim <joakim.snokvist@gmail.com>
2025-12-19 17:51:22 +0100

    Update pixelpilot_mini_rk.mk

--- package/pixelpilot_mini_rk/pixelpilot_mini_rk.mk
+++ package/pixelpilot_mini_rk/pixelpilot_mini_rk.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PIXELPILOT_MINI_RK_VERSION = main
+PIXELPILOT_MINI_RK_VERSION = 25189fb6a875929c7f9942441c64e2840ee804a0
 PIXELPILOT_MINI_RK_SITE = https://github.com/snokvist/pixelpilot_mini_rk.git
 PIXELPILOT_MINI_RK_SITE_METHOD = git
 PIXELPILOT_MINI_RK_LICENSE = Proprietary
