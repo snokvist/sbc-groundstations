710d2096c02f308f3b0036a639557140f2fd754e
Joakim <joakim.snokvist@gmail.com>
2025-12-20 14:54:09 +0100

    Update wfb_supervisor.mk

--- package/wfb_supervisor/wfb_supervisor.mk
+++ package/wfb_supervisor/wfb_supervisor.mk
@@ -21,7 +21,7 @@ define WFB_SUPERVISOR_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc $(WFB_SUPERVISOR_PKGDIR)/files/wfb.conf
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc $(WFB_SUPERVISOR_PKGDIR)/files/wifi_supervisor.conf
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc $(WFB_SUPERVISOR_PKGDIR)/files/hostapd.conf
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc $(WFB_SUPERVISOR_PKGDIR)/files/dhcpcd.conf
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc $(WFB_SUPERVISOR_PKGDIR)/files/dhcpd.conf
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
 	$(INSTALL) -m 0755 -t $(TARGET_DIR)/etc/init.d $(WFB_SUPERVISOR_PKGDIR)/files/S50ap
