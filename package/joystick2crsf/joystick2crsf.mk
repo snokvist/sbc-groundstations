ef89275a2d442760a405235fc8fbd7a99a1cfa2f
Joakim <joakim.snokvist@gmail.com>
2025-12-28 12:14:42 +0100

    Update joystick2crsf.mk

--- package/joystick2crsf/joystick2crsf.mk
+++ package/joystick2crsf/joystick2crsf.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-JOYSTICK2CRSF_VERSION = 8847f04c21b8f0dfefbff1ee35da916cc00e2621
+JOYSTICK2CRSF_VERSION = 92942d76b69478e88a942942b930d24c8a703acb
 JOYSTICK2CRSF_SITE = https://github.com/snokvist/joystick2crsf.git
 JOYSTICK2CRSF_SITE_METHOD = git
 JOYSTICK2CRSF_LICENSE = Proprietary
