c73d2f36e357a6fd9adb38720996b37b72c2d80f
snokvist <joakim.snokvist@gmail.com>
2026-08-01 20:57:33 +0200

    package: disable unrotated link stats logging

--- package/waybeam-link/waybeam-link.mk
+++ package/waybeam-link/waybeam-link.mk
@@ -12,7 +12,7 @@
 # authenticate to the private repo. Mirrors the waybeam-hub packaging pattern.
 #
 ###############################################################################
-WAYBEAM_LINK_VERSION = 223c27984c4a21beb04e2f90d52ac03c1ba775cf
+WAYBEAM_LINK_VERSION = 5c2bba8d7beeb80d1cb44530084e35b004f36fff
 WAYBEAM_LINK_SITE = $(call github,snokvist,waybeam-link,$(WAYBEAM_LINK_VERSION))
 WAYBEAM_LINK_LICENSE = GPL-2.0-or-later
 WAYBEAM_LINK_LICENSE_FILES = LICENSE
